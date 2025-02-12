c818def765edc59783010e031009fe8757af20e1
Hirtheesh1 <150221763+Hirtheesh1@users.noreply.github.com>
2025-02-12 22:04:08 +0530

    some what frontend was done

---
+++ src/Create.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './create.css'
+export default function Create() {
+  return (
+    <div className='create_form'>
+        <input type='text' name='' id=''/>
+        <button type='button'>Add</button>
+    </div>
+  )
+}
